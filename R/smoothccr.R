26bbc09a4be776fabffe40481f7bfcc5f42f9306
Jonas Bhend <jonas.bhend@meteoswiss.ch>
2014-11-03 08:58:27 +0100

    Changed name to smoothccr for daily ccr with smoothing

---
+++ R/smoothccr.R
@@ -0,0 +1,47 @@
+#' smoothccr
+#' 
+#' Computes climate conserving recalibration on daily data with smoothing for climatology
+#' 
+#' @param fcst n x m x k array of n lead times, m forecasts, of k ensemble members
+#' @param obs n x m matrix of veryfing observations
+#' @param fcst.out array of forecast values to which bias correction
+#' should be applied (defaults to \code{fcst})
+#' @param span the parameter which controls the degree of smoothing (see \code{\link{loess}})
+#' @param ... additional arguments for compatibility with other bias correction methods
+#' 
+#' @examples
+#' fcst <- array(rnorm(3000*1*51, mean=1, sd=rep(seq(0.5,2, length=3000), each=1)), 
+#' c(1, 3000, 51)) + 0.5*sin(seq(0,4,length=1))
+#' obs <- array(rnorm(3000, mean=2), c(1, 3000)) + sin(seq(0,4, length=1))
+#' fcst.debias <- biascorrection:::ccr(fcst, obs, span=0.5)
+#' f.rmse <- sqrt(apply((obs - apply(fcst.debias, 2, mean))**2, 1, mean))
+#' f.sd <- sqrt(mean(apply(fcst.debias, 2, sd)**2))
+#' f.sd / f.rmse
+#' bias <- mean(fcst.debias - obs[1,])
+#' 
+#' @keywords util
+smoothccr <- function(fcst, obs, fcst.out=fcst, span=min(1, 31/nrow(fcst)), ...){
+  fcst.ens <- rowMeans(fcst, dims=2)
+  fcst.ens[is.na(obs)] <- NA
+  ## compute climatology
+  fcst.mn <- rowMeans(fcst.ens, dims=1, na.rm=T)
+  obs.mn <- rowMeans(obs, dims=1, na.rm=T)
+  fcst.clim <- sloess(fcst.mn, span=span)
+  obs.clim <- sloess(obs.mn, span=span)
+  
+  ## compute CCR prerequisites (Notation as in Weigel et al. 2009)
+  x <- obs - obs.clim
+  sig_x <- sqrt(apply(x**2, 1, mean))
+  fi <- fcst - fcst.clim
+  mu_f <- rowMeans(fi, dims=2)
+  sig_mu <- sqrt(apply(mu_f**2, 1, mean))
+  sig_ens <- sqrt(apply(apply(fi, 1:2, sd)**2, 1, mean))
+  rho <- diag(cor(t(mu_f), t(x)))
+  rr <- rho*sig_x / sig_mu
+  ss <- sqrt(1 - rho**2) * sig_x / sig_ens
+  ## put everything back together (with de-biasing)
+  fi_out <- fcst.out - fcst.clim
+  mu_fout <- rowMeans(fi_out, dims=2)
+  fi_ccr <- as.vector(rr * mu_fout) + ss * (fi_out - as.vector(mu_fout)) + obs.clim  
+  return(fi_ccr)
+}
