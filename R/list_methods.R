0b7d966ed886ac47371990e9b6207b48399d5087
Jonas Bhend <jonas.bhend@meteoswiss.ch>
2017-01-24 09:49:22 +0100

    nice formatting of list_methods()

--- R/list_methods.R
+++ R/list_methods.R
@@ -1,6 +1,11 @@
-#' List Calibration Methods
+#' @name list_methods
+#' @aliases print.biascorrection
 #' 
-#' List calibration methods available in \code{biascorrection}
+#' @title List Calibration Methods
+#' 
+#' @description 
+#' \code{list_methods} lists calibration methods available in \code{biascorrection}
+#' and \code{print.biascorrection} is used to nicely format these for display.
 #' 
 #' @examples
 #' list_methods()
@@ -13,5 +18,15 @@ list_methods <- function(){
   m.i <- which(methods %in% c('monmean', 'biascorrection', 'sloess', 'debias', 'list_methods', 'cor', 'debiasApply'))
   m.i <- c(m.i, which(methods[m.i + 1] == ""), which(desc == "Forecast Data Sets"))
   mout <- data.frame(METHODS=methods, DESCRIPTION=desc)[-m.i,]
+  class(mout) <- c('biascorrection')
   return(mout)
+}
+
+#' @rdname list_methods
+#' @param df data frame of methods to be used with \code{\link{debias}}
+
+print.biascorrection <- function(df){
+  df$METHODS <- paste0(df$METHODS, ' ')
+  class(df) <- 'data.frame'
+  print.data.frame(df, right=FALSE, row.names=FALSE)
 }
